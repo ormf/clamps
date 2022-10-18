522f5a12dd29016b0aeb2b100caafd77c855191f
ormf <junk14@selma.hfmdk-frankfurt.de>
2022-10-18 11:30:46 +0200

    added cm-sampleevt and cl-poolplayer-extra

---
+++ cl-poolplayer-extra.lisp
@@ -0,0 +1,52 @@
+;;; 
+;;; cl-poolplayer-extra.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2021 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(in-package :cl-poolplayer)
+
+
+(defun cm-collect-song (song)
+  (let ((*events* '())
+        (time (now)))
+    (funcall (song-playfn song)
+             (funcall (song-durfn song))
+             :player-type 'eventplotter)
+    (sort
+     (mapcar #'(lambda (x) (apply #'make-instance 'cm::poolevt
+                             :time (float (- (first x) time) 1.0)
+                             (cdr x)
+                             ;; :lsample (getf (cdr x) :lsample)
+                             ;; (progn (remf (cdr x) :buffer)
+                             ;;        (cdr x))
+                             ))
+             *events*)
+     #'< :key (lambda (x) (sv x cm::time)))))
+
+(defmacro collecting-cm (&rest body)
+  `(let ((*events* '())
+         (time (now)))
+     ,@body
+     (sort
+      (mapcar #'(lambda (x) (apply #'make-instance 'cm::poolevt
+                             :time (float (- (first x) time) 1.0)
+                             (cdr x)))
+              *events*)
+      #'< :key (lambda (x) (sv x cm::time)))))
+
+(export '(cm-collect-song collecting-cm) 'cl-poolplayer)
