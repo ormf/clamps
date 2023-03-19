ef6911a4542f7497acde8192d60e535a8bd99444
ormf <junk14@selma.hfmdk-frankfurt.de>
2023-03-19 20:13:35 +0100

    updated submodules

--- src/cm-all.lisp
+++ src/cm-all.lisp
@@ -77,7 +77,7 @@ supplied and gets interned as a parameter."
 
 (unless (cm::rts?) (rts))
 ;;(make-mt-stream *mt-out01* *midi-out1* '(4 0))
-;;(restart-qsynth)
+(restart-qsynth)
 ;;(setf *rts-out* *mt-out01*)
 (format t "~&midi initialized!~%")
 (swank:eval-in-emacs `(load ,(namestring (asdf:system-relative-pathname 'cm-all "elisp/incudine-hush.el"))))
