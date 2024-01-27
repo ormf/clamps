bbd8e14b327a707f7f94fc3b6ca7dc61849c7f6d
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-01-27 15:54:10 +0100

    full example.

--- examples/example.lisp
+++ examples/example.lisp
@@ -56,17 +56,17 @@
 (progn
   (clear-bindings)
   (setf x (make-ref 0.5))
-  ;; (setf x-bang (make-bang (lambda () (set-val x 0))))
-  ;; (setf x-db
-  ;;       (make-computed
-  ;;        (lambda () (clip (round (rms->db (get-val x))) -40 0))
-  ;;        (lambda (val) (%set-val x (clip (float (if (<= val -40) 0 (db->rms val))) 0 1)))))
-  ;; (setf radio
-  ;;       (make-computed
-  ;;        (lambda () (round (/ (+ 40 (get-val x-db)) 40/7)))
-  ;;        (lambda (val) (%set-val x-db (- (float (* val 40/7)) 40)))))
-  ;; (setf mslider (make-array 8 :initial-contents (loop repeat 8 collect (make-ref 0))))
-  ;; (setf (aref mslider 0) x)
+  (setf x-bang (make-bang (lambda () (set-val x 0))))
+  (setf x-db
+        (make-computed
+         (lambda () (clip (round (rms->db (get-val x))) -40 0))
+         (lambda (val) (%set-val x (clip (float (if (<= val -40) 0 (db->rms val))) 0 1)))))
+  (setf radio
+        (make-computed
+         (lambda () (round (/ (+ 40 (get-val x-db)) 40/7)))
+         (lambda (val) (%set-val x-db (- (float (* val 40/7)) 40)))))
+  (setf mslider (make-array 8 :initial-contents (loop repeat 8 collect (make-ref 0))))
+  (setf (aref mslider 0) x)
   nil)
 
 ;;; Define our CLOG application
@@ -82,26 +82,26 @@
   "On-new-window handler."
   (setf (title (html-document body)) "Gui Test")
   (let ((collection (create-collection body "1/2")))
-   ;; (create-o-vumeter collection (bind-ref-to-attr x-db "db-value") :mapping :pd)
+    (create-o-vumeter collection (bind-ref-to-attr x-db "db-value") :mapping :pd)
     (create-o-numbox collection (bind-ref-to-attr x "value") 0 1 :precision 2
                      :css '(:height 1em))
     (create-o-knob collection (bind-ref-to-attr x "value") 0 1 0.01)
- ;; (create-o-knob collection (bind-ref-to-attr x "value") 0 1 0.01)
-   ;;  (create-o-knob collection (bind-ref-to-attr x-db "value") -40 0 1 :unit "dB" :precision 0)
-   ;;  (create-o-bang collection (bind-ref-to-attr x-bang "bang")
-   ;;                 :css '(:height 1em))
-   ;;  (create-o-toggle collection (bind-ref-to-attr x "value")
-   ;;                   :css '(:height 1em))
-   ;;  (create-o-radio collection (bind-ref-to-attr radio "value")
-   ;;                  :direction :up
-   ;;                  :background '(("#444" "#888") ("orange"))
-   ;;                  :labels (list (loop for num below 8 collect num)))
-   ;;  (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
-   ;;  (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
-   ;;  (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
-   ;;  (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
-   ;;  (create-o-multislider collection (bind-ref-to-attr mslider "value")
-   ;;                        :colors '("#8f8" "#f88" "#44f") :background "transparent")
+ (create-o-knob collection (bind-ref-to-attr x "value") 0 1 0.01)
+    (create-o-knob collection (bind-ref-to-attr x-db "value") -40 0 1 :unit "dB" :precision 0)
+    (create-o-bang collection (bind-ref-to-attr x-bang "bang")
+                   :css '(:height 1em))
+    (create-o-toggle collection (bind-ref-to-attr x "value")
+                     :css '(:height 1em))
+    (create-o-radio collection (bind-ref-to-attr radio "value")
+                    :direction :up
+                    :background '(("#444" "#888") ("orange"))
+                    :labels (list (loop for num below 8 collect num)))
+    (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
+    (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
+    (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
+    (create-o-slider collection (bind-ref-to-attr x "value") :background "transparent")
+    (create-o-multislider collection (bind-ref-to-attr mslider "value")
+                          :colors '("#8f8" "#f88" "#44f") :background "transparent")
     ))
 
 ;;; We don't want to restart the server everytime when the new-window
