c6d99c7906a5453952c2a9a12478da5ac3ea6c2b
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-04-28 14:24:51 +0200

    added scope and scop-gui

---
+++ scope-gui.lisp
@@ -0,0 +1,113 @@
+;;; 
+;;; levelmeter-gui.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2024 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(in-package :clog-dsp-widgets)
+
+(defclass scope (cuda-dsp)
+  ((scope-type :initform :bus :initarg :type :accessor meter-type
+               :type (member :in :out :bus))
+   (refs :initform nil :initarg :refs :accessor refs)
+   (size :initform nil :initarg :size :accessor size)
+   (audio-bus :initform 0 :initarg :audio-bus :accessor audio-bus)))
+
+(defmethod initialize-instance :after ((instance scope) &rest initargs)
+  (declare (ignorable initargs))
+  (with-slots (refs scope-type num nodes node-group audio-bus) instance
+    (unless refs
+      (setf refs (make-array num
+                             :initial-contents
+                             (loop repeat num collect (make-ref 0.0d0)))))
+;;;    (incudine.util:msg :warn "heyho")
+    (case meter-type
+      (:bus
+       (meters-dsp :id-callback (lambda (id) (push id nodes)) :freq 10 :num num :refs refs :audio-bus audio-bus :group node-group))
+      (:in
+       (inmeters-dsp :id-callback (lambda (id) (push id nodes)) :freq 10 :num num :refs refs :audio-bus audio-bus :group node-group))
+      (:out
+       (outmeters-dsp :id-callback (lambda (id) (push id nodes)) :freq 10 :num num :refs refs :audio-bus audio-bus :group node-group)))))
+
+(defun bus-scope-gui (id gui-parent &key (group 300) (amp (make-ref 1.0d0)) refs (num 1) (audio-bus 0) (channel-offset 0)
+                                                  (create-bus t))
+  (let* ((dsp
+           (or (find-dsp id)
+               (let ((new (add-dsp 'master-bus-levelmeter :id id :node-group group
+                                                          :audio-bus audio-bus :refs refs :num num
+                                                          :channel-offset channel-offset
+                                                          :create-bus create-bus)))
+                 (loop until (bus-node new))
+                 (push (watch (lambda () (set-control (bus-node new) :amp (get-val amp)))) (unwatch new))
+                 new))))
+    (with-slots (refs bus-node) dsp
+      (let* ((gui-container (create-div gui-parent
+                                        :class "levelmeter-panel"
+                                        :css `(:display "flex" :border "0.1em solid black" :background "transparent" :height "12em" :width "6em" :margin-left "0.1em" :margin-right "0.1em"))))
+        (dotimes (idx num)
+          (create-o-vumeter
+           gui-container
+           (bind-refs-to-attrs (aref refs idx) "db-value")
+           :mapping :pd
+           :css '(:height "97%" :width "0.25em" :min-width "0.1em" :margin "1.75% 1.75% 1.5% 1.5%" :border "0.1em solid black" :background "#222")))
+        (create-o-slider
+         gui-container
+         (bind-refs-to-attrs amp "value")
+         :css '(:height "97%" :width "0.5em" :min-width "0.1em" :margin "1.75% 1.75% 1.5% 1.5%" :border "0.1em solid black" :background "#999"))))))
+
+#|
+
+
+
+(defparameter *in-refs* (coerce (loop repeat 8 collect (cl-refs:make-ref -100)) 'vector))
+(defparameter *out-refs* (coerce (loop repeat 8 collect (cl-refs:make-ref -100)) 'vector))
+
+(defun setup-meters ()
+  (dolist (id '(:lm-in :lm-out))
+    (remove-dsp id))
+  (setup-io)
+  (add-dsp 'levelmeter :id :lm-in :type :in :num 8 :node-group 100 :refs *in-refs*)
+  (add-dsp 'levelmeter :id :lm-out :type :out :num 8 :node-group 300 :refs *out-refs*)
+;;;  (add-dsp 'levelmeter :id :lm-out :type :bus :num 8 :audio-bus 8 :node-group 300 :refs *out-refs*)
+  )
+
+(defun meters-window (body)
+  "handler for /meters"
+  (setf (title (html-document body)) "Meters")
+  (setf (style body :display) "flex")
+  (levelmeter-gui :lm-in body :type :in :group 100 :num 8)
+  (levelmeter-gui :lm-out body :type :out :group 300 :num 8))
+
+(set-on-new-window #'meters-window :path "/meters" :boot-file "/start.html")
+
+(defparameter *open-meter-guis* (make-ref 0))
+|#
+
+#|
+
+(dump (node 0))
+*in-refs*
+*out-refs*
+(setup-meters)
+(find-dsp :lm-in)
+(remove-dsp :lm-in)
+(remove-dsp :lm-in)
+(node-free-all)
+
+(incudine::sin-test 411 0.05 :id 101 :head 200)
+
+|#
---
+++ scope-incudine.lisp
@@ -0,0 +1,152 @@
+;;; 
+;;; scope-incudine.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2024 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(in-package #:incudine)
+(export '(scope-dsp) :incudine)
+
+(define-vug loop-counter (max)
+  (with-samples
+      ((phase -1.0d0))
+    (prog1
+        (incf phase)
+      (cond ((>= phase (- max 1.0d0)) (setf phase -1.0d0))))))
+
+#|
+(define-vug scope-vug ((in channel-number) freq (ref cl-refs:ref-object))
+  (:defaults 0 10 nil)
+  (with ((size (round-sample (/ *sample-rate* freq)))
+         (bufsize 100)
+         (buffer (make-buffer bufsize)))
+    (declare (alexandria:non-negative-fixnum size bufsize))
+    (foreach-frame
+      (let ((idx (round-sample (loop-counter size))))
+        (incudine.util:nrt-msg :info "~a ~a" idx current-frame)
+        (cond
+          ((= idx 1) (incudine.util:nrt-msg :info "bingo ~a" idx)
+           (cl-refs:set-val ref buffer))
+          ((< idx bufsize) (set-buffer-value buffer idx (audio-out in current-frame))))))))
+|#
+
+(defun copy-buffers (src dest)
+  "Return a copy of BUFFER."
+  (declare (type buffer src dest))
+  (if (free-p src)
+      (incudine-error "The buffer is unusable.")
+      (progn
+        (foreign-copy-samples (buffer-data dest) (buffer-data src)
+                              (buffer-size src))
+;;;      (copy-struct-slots buffer (file textfile-p) src dest)
+        dest)))
+
+(define-vug scope-vug ((in channel-number) freq (ref cl-refs:ref-object))
+  (:defaults 0 10 nil)
+  (with ((size (round-sample (/ *sample-rate* freq)))
+         (bufsize (incudine:buffer-size (cl-refs:get-val ref)))
+         (buffer (make-buffer bufsize)))
+    (declare (alexandria:non-negative-fixnum size bufsize)
+             (buffer buffer))
+    (foreach-frame
+      (let ((idx (round-sample (loop-counter size))))
+        ;;;(incudine.util:nrt-msg :info "~a ~a" idx current-frame)
+        (when (zerop idx)
+          ;;          (incudine.util:nrt-msg :info "bingo ~a" idx)
+          (let ((gui-buf (cl-refs:get-val ref)))
+            (copy-buffers buffer gui-buf)
+            (at (now) (lambda () (cl-refs:set-val ref gui-buf :force t)))))
+        (when (< idx bufsize) (set-buffer-value buffer idx (audio-out in current-frame)))))))
+
+(define-vug in-scope-vug ((in channel-number) freq (ref cl-refs:ref-object))
+  (:defaults 0 10 nil)
+  (with ((size (round-sample (/ *sample-rate* freq)))
+         (bufsize (incudine:buffer-size (cl-refs:get-val ref)))
+         (buffer (make-buffer bufsize)))
+    (declare (alexandria:non-negative-fixnum size bufsize)
+             (buffer buffer))
+    (foreach-frame
+      (let ((idx (round-sample (loop-counter size))))
+        ;;;(incudine.util:nrt-msg :info "~a ~a" idx current-frame)
+        (when (zerop idx)
+          ;;          (incudine.util:nrt-msg :info "bingo ~a" idx)
+          (let ((gui-buf (cl-refs:get-val ref)))
+            (copy-buffers buffer gui-buf)
+            (at (now) (lambda () (cl-refs:set-val ref gui-buf :force t)))))
+        (when (< idx bufsize) (set-buffer-value buffer idx (audio-in in current-frame)))))))
+
+(define-vug out-scope-vug ((out channel-number) freq (ref cl-refs:ref-object))
+  (:defaults 0 10 nil)
+  (with ((size (round-sample (/ *sample-rate* freq)))
+         (bufsize (incudine:buffer-size (cl-refs:get-val ref)))
+         (buffer (make-buffer bufsize)))
+    (declare (alexandria:non-negative-fixnum size bufsize)
+             (buffer buffer))
+    (foreach-frame
+      (let ((idx (round-sample (loop-counter size))))
+        ;;;(incudine.util:nrt-msg :info "~a ~a" idx current-frame)
+        (when (zerop idx)
+          ;;          (incudine.util:nrt-msg :info "bingo ~a" idx)
+          (let ((gui-buf (cl-refs:get-val ref)))
+            (copy-buffers buffer gui-buf)
+            (at (now) (lambda () (cl-refs:set-val ref gui-buf :force t)))))
+        (when (< idx bufsize) (set-buffer-value buffer idx (audio-out out current-frame)))))))
+
+(define-vug bus-scope-vug ((chan channel-number) freq (ref cl-refs:ref-object))
+  (:defaults 0 10 nil)
+  (with ((size (round-sample (/ *sample-rate* freq)))
+         (bufsize (incudine:buffer-size (cl-refs:get-val ref)))
+         (buffer (make-buffer bufsize)))
+    (declare (non-negative-fixnum size bufsize)
+             (buffer buffer))
+    (foreach-frame
+      (let ((idx (round-sample (loop-counter size))))
+        (declare (type non-negative-fixnum idx))
+        ;;;(incudine.util:nrt-msg :info "~a ~a" idx current-frame)
+        (when (zerop idx)
+          ;;          (incudine.util:nrt-msg :info "bingo ~a" idx)
+          (reduce-warnings
+            (let ((gui-buf (cl-refs:get-val ref)))
+              (copy-buffers buffer gui-buf)
+              (at (now) (lambda () (cl-refs:set-val ref gui-buf :force t))))))
+        (when (< idx bufsize) (set-buffer-value (the buffer buffer)
+                                                (the non-negative-fixnum idx)
+                                                (incudine::audio-bus chan current-frame)))))))
+
+(dsp! in-scope ((chan channel-number) freq (ref cl-refs:ref-object))
+   (:defaults 0 nil (cl-refs:make-ref (make-buffer 100)))
+  (foreach-frame (in-scope-vug chan freq ref)))
+
+(dsp! out-scope ((chan channel-number) freq (ref cl-refs:ref-object))
+   (:defaults 0 nil (cl-refs:make-ref (make-buffer 100)))
+  (foreach-frame (out-scope-vug chan freq ref)))
+
+(dsp! bus-scope ((chan channel-number) freq (ref cl-refs:ref-object))
+   (:defaults 0 nil (cl-refs:make-ref (make-buffer 100)))
+  (foreach-frame (bus-scope-vug chan freq ref)))
+
+#|
+(defun scope-dsp (&key (group 300) (num 1)
+                    id-callback refs (freq 5) (audio-bus 0))
+  (loop
+    for idx below num
+    do (progn
+         (scope (+ audio-bus idx) freq (aref refs idx)
+                :action (lambda (n)
+                          (funcall id-callback (node-id n)))
+                :tail group))))
+|#
