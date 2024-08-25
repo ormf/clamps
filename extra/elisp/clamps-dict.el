c4bd352bb74de724ccf1cb579075eae1ba72282c
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-08-25 19:54:29 +0200

    fixed load-path in clamps-dict.el

--- extra/elisp/clamps-dict.el
+++ extra/elisp/clamps-dict.el
@@ -1,4 +1,4 @@
-(load "/home/orm/work/programmieren/lisp/clamps/doc/../extra/elisp/cm-dict.el")
+(load "~/quicklisp/local-projects/clamps/extra/elisp/cm-dict.el")
 (mapcar
  (lambda (entry)
    (let ((symbol (intern (car entry)
