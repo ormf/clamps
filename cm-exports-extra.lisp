34b99beeb27f8505c0b3338dc99e38c5628d5fc6
ormf <junk14@selma.hfmdk-frankfurt.de>
2025-09-03 18:40:37 +0200

    added cm-exports-extra.lisp

---
+++ cm-exports-extra.lisp
@@ -0,0 +1,23 @@
+;;; 
+;;; cm-exports-extra.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2025 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(in-package :cm)
+
+(export '(container-subobjects) :cm)
